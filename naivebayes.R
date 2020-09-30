9b1cd06f404cb53e25b43320f138990de5a69918
manasi10 <40830209+manasi10@users.noreply.github.com>
2020-09-30 21:20:57 +0530

    Add files via upload

---
+++ naivebayes.R
@@ -0,0 +1,117 @@
+setwd("D:/MBA_3RD SEM/ML")
+
+install.packages("mlbench")
+library(mlbench)
+data("HouseVotes84") #this dataset is contained in mlbench library
+
+#barplots for specific issue
+plot(as.factor(HouseVotes84[,2]))
+title(main='Votes cast for issue 1', xlab ="vote", ylab ="Num reps")
+
+#by party
+HouseVotes84$Class
+Repub <- HouseVotes84$Class=="republican"
+Democrat <- HouseVotes84$Class=="democrat"
+Repub
+plot(as.factor(HouseVotes84[Repub,2]))
+title(main="Republican votes cast for issue 1", xlab="vote", ylab="Num reps")
+plot(as.factor(HouseVotes84[Democrat,2]))
+title(main="Democrat votes cast for issue 1", xlab="vote", ylab="Num reps")
+
+
+#We have NA values, we will impute the values
+#functions needs imputation
+
+#function to return numbers of NAs by vote and class (democrat or republican)
+
+na_by_col_class <- function(col,cls) {
+  return(sum(is.na(HouseVotes84[,col]) & HouseVotes84$Class==cls))
+}
+na_by_col_class
+
+#function to compute the conditional probability that a member of a party will cast a "yes" vote
+#a particular issue. The probability is based on all members of the party who actually cast a vote on issue
+
+p_y_col_class <- function(col,cls){
+  sum_y <- sum(HouseVotes84[,col]=="y" & HouseVotes84$Class==cls, na.rm==TRUE)
+  sum_n <- sum(HouseVotes84[,col]=="n" & HouseVotes84$Class==cls, na.rm==TRUE)
+}
+
+
+#check the prob of yes vote by a democrat in issue 5
+p_y_col_class(5,"democrat")
+
+#check the prob of yes vote by a republican in issue 5
+p_y_col_class(5,"republican")
+
+#check the NA for both Democrat and republican
+na_by_col_class(2, "democrat")
+na_by_col_class(2,"republican")
+
+#impute missing values
+#if the republican congresman di'nt vote, then we are allocating 'y' or 'n' based on if their
+#party voted 'y' or 'n'
+
+for(i in 2:ncol(HouseVotes84)){
+  if(sum(is.na(HouseVotes84[,i])>0)) {
+    c1 <- which(is.na(HouseVotes84[,i])& HouseVotes84$Class=="democrat", arr.ind = TRUE)
+    c2 <- which(is.na(HouseVotes84[,i])& HouseVotes84$Class=="republican", arr.ind = TRUE)
+    HouseVotes84[c1,i] <- 
+      ifelse(runif(na_by_col_class(i,"democrat")) <p_y_col_class(i,"democrat"), "y", "n")
+      HouseVotes84[c2,i] <- 
+        ifelse(runif(na_by_col_class(i,"republican")) <p_y_col_class(i,"republican"), "y", "n")
+  }
+}
+
+
+#divide into test and train sets
+#create new col "train" and assign 1 or 0 in 80/20 proportion via random unitform dist
+
+HouseVotes84[,"train"] <- ifelse(runif(nrow(HouseVotes84))<0.80 , 1 , 0)
+
+#get col number of train or test indicator col
+trainColNum <- grep("train", names(HouseVotes84))
+
+#separate training and test sets and remove training column before modeling
+trainHouseVotes84 <- HouseVotes84[HouseVotes84$train==1, -trainColNum]
+testHouseVotes84 <- HouseVotes84[HouseVotes84$train==0, -trainColNum]
+
+
+#install package e1071
+install.packages("e1071")
+library(e1071)
+
+#Naive Bayes
+nb_model <- naiveBayes(Class~.,data = trainHouseVotes84)
+
+#in this notation, the dependent variable (to be predicted) appears on the LHS of the ~ and the independent is on the right side
+nb_model
+summary(nb_model)
+str(nb_model)
+
+#test the model
+nb_test_predict <- predict(nb_model,testHouseVotes84[,-1])
+
+#Fraction of correct predictions
+mean(nb_test_predict==testHouseVotes84$Class)
+
+#Confusion matrix
+table(pred=nb_test_predict,true=testHouseVotes84$Class)
+
+# Function to create,run and record model results
+nb_multiple_runs <- function(train_fraction,n){
+  fraction_correct <- rep(NA,n)
+  for (i in 1:n){
+    HouseVotes84[,'train'] <- ifelse(runif(nrow(HouseVotes84))<train_fraction,1,0)
+    trainColNum <- grep('train',names(HouseVotes84))
+    trainHouseVotes84 <- HouseVotes84[HouseVotes84$train==1,-trainColNum]
+    testHouseVotes84 <- HouseVotes84[HouseVotes84$train==0,-trainColNum]
+    nb_model <- naiveBayes(Class~.,data = trainHouseVotes84)
+    nb_test_predict <- predict(nb_model,testHouseVotes84[,-1])
+    fraction_correct[i] <- mean(nb_test_predict==testHouseVotes84$Class)
+  }
+  return(fraction_correct)
+}
+
+
+
